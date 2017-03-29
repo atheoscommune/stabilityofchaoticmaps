263a71cdf1454b7155889ab9bad5af97ecf5a732
Prince <atheos.commune@gmail.com>
2017-03-29 23:31:59 +0530

    Completed Crypto

--- java/crypto/Test.java
+++ java/crypto/Test.java
@@ -4,25 +4,36 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 
 import javax.imageio.ImageIO;
+
 public class Test {
 	public static void main(String[] args) {
 		try {
-			int x = 1;
+			int x = 0;
 			if (x == 0) {
-				File file = new File(ChaosImageEncrypt.class.getClassLoader().getResource("box.bmp").getPath());
+				File file = new File(ChaosImageEncrypt.class.getClassLoader().getResource("large.bmp").getPath());
 				BufferedImage img = ImageIO.read(file);
 
-				String pwd = "5555555555";
-				MultipleIterationImageEncryption lme = new MultipleIterationImageEncryption();
+				String pwd = "aaaaaaaaaa";
+				ChaosImageEncryptModified lme = new ChaosImageEncryptModified();
 
 				lme.encryptImage(pwd, img, 0, "enc.bmp");
+
+				// ImageIO.write(img, ".bmp", new File("my"));
+				// JFrame frame = new JFrame();
+				// frame.getContentPane().setLayout(new FlowLayout());
+				// frame.getContentPane().add(new JLabel(new ImageIcon(img)));
+				// frame.pack();
+				// frame.setVisible(true);
+				
+				
+				
 			} else {
 				{
 					File file = new File(ChaosImageEncrypt.class.getClassLoader().getResource("enc.bmp").getPath());
 					BufferedImage img = ImageIO.read(file);
 
-					String pwd = "5555555555";
-					MultipleIterationImageEncryption lme = new MultipleIterationImageEncryption();
+					String pwd = "aaaaaaaaaa";
+					ChaosImageEncryptModified lme = new ChaosImageEncryptModified();
 
 					lme.encryptImage(pwd, img, 1, "dec.bmp");
 				}
